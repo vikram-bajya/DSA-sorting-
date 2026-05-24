23f7c0e331b10ff9c445d08db5ec141b4e6933c4
Vikram Bajya <vjat39915@gmail.com>
2026-05-24 22:55:23 +0530

    Bubble sorting

---
+++ bubbleSort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void print(int arr[],int n){
+    for (int i = 0; i < n; i++){
+        
+        cout<<arr[i]<<" ";
+    }
+    
+    
+}
+
+
+void bubbleSort(int arr[],int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+        
+    }
+ print(arr,n);
+    
+}
+
+int main (){
+
+    int arr[5]={5,4,1,3,2};
+  
+    bubbleSort( arr,5);
+    return 0;
+
+}
+
