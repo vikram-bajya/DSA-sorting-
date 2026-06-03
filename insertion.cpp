1e92e149fb5ac7cfc38e99bff1445b50bb15ee4b
Vikram Bajya <vjat39915@gmail.com>
2026-06-03 16:23:15 +0530

    insertion sort

--- insertion.cpp
+++ insertion.cpp
@@ -1,40 +1,40 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// void print(int arr[],int n){
-//     for (int i = 0; i < n; i++)
-//     {
-//         cout<<arr[i];
-//     }
+void print(int arr[],int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i];
+    }
     
-// }
-// void insertion(int arr[],int n){
-//     for (int  i = 1; i < n; i++){
-//         int curr=arr[i];
-//         int j=i-1;
+}
+void insertion(int arr[],int n){
+    for (int  i = 1; i < n; i++){
+        int curr=arr[i];
+        int j=i-1;
 
-//         while (j>=0 && arr[j]>curr)
-//         {
-//             //arr[j+1]=arr[j];
-//             //or
-//             swap(arr[j],arr[j+1]);
-//             j--;
-//         }
-//         arr[j+1]=curr;
+        while (j>=0 && arr[j]>curr)
+        {
+            //arr[j+1]=arr[j];
+            //or
+            swap(arr[j],arr[j+1]);
+            j--;
+        }
+        arr[j+1]=curr;
         
-//     }
+    }
     
-//     print (arr,n);
+    print (arr,n);
     
 
-// }
+}
 
-// int main(){
-//     int arr[5]={5,4,1,3,2};
-//     int n=sizeof(arr)/sizeof(int);
-//     insertion(arr,n);
+int main(){
+    int arr[5]={5,4,1,3,2};
+    int n=sizeof(arr)/sizeof(int);
+    insertion(arr,n);
 
-//     return 0;
-// }
+    return 0;
+}
 
 
